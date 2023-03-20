b84893469750df59e0d0fed32dd3d90df7e827cd
Artur Sady <asady502@gmail.com>
2023-03-20 16:36:44 +0100

    Initial commit

---
+++ src/test/java/com/sda/springNauka/SpringNaukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.springNauka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNaukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
