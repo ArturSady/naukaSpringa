76e6b2846ad970bd5219fd9c8a0ea779100ab25d
Artur Sady <asady502@gmail.com>
2023-03-20 16:52:08 +0100

    implement Application class

---
+++ src/main/java/com/sda/springNauka/Application.java
@@ -0,0 +1,12 @@
+package com.sda.springNauka;
+
+public class Application {
+
+    public void start(){
+        System.out.println("Start programu");
+    }
+
+    public void end(){
+        System.out.println("Koniec programu");
+    }
+}
--- src/main/java/com/sda/springNauka/SpringNaukaApplication.java
+++ src/main/java/com/sda/springNauka/SpringNaukaApplication.java
@@ -3,11 +3,18 @@ package com.sda.springNauka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringNaukaApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringNaukaApplication.class, args);
-	}
 
+
+		//public static void main(String[] args) {
+		//	SpringApplication.run(SpringNaukaApplication.class, args);
+		//}
+		Application application = new Application();
+
+		application.start();
+		application.end();
+
+	}
 }
