cec599c84fac7867b31964526412043bc74cd942
Artur Sady <asady502@gmail.com>
2023-03-20 17:21:52 +0100

    Class FileRepository implemented

--- src/main/java/com/sda/springNauka/Application.java
+++ src/main/java/com/sda/springNauka/Application.java
@@ -2,11 +2,16 @@ package com.sda.springNauka;
 
 public class Application {
 
+private final FileRepository fileRepository = new FileRepository();
     public void start(){
         System.out.println("Start programu");
+        fileRepository.read();
+
     }
 
     public void end(){
         System.out.println("Koniec programu");
+        fileRepository.write();
+
     }
 }
