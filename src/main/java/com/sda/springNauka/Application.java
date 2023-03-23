89fd5f6258885745787764532572096d90e9ffe7
Artur Sady <asady502@gmail.com>
2023-03-23 17:18:56 +0100

    add @Autowired example and @PostConstruct

--- src/main/java/com/sda/springNauka/Application.java
+++ src/main/java/com/sda/springNauka/Application.java
@@ -1,5 +1,7 @@
 package com.sda.springNauka;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -7,19 +9,26 @@ import org.springframework.stereotype.Component;
 public class Application {
 
 //private final FileRepository fileRepository = new FileRepository();
-    private final Repository repository;
-
+    //@Autowired
+    private  Repository repository;
+@Autowired
     public Application(@Qualifier("File") Repository repository){
         System.out.println("Utworzenie application");
         System.out.println(repository);
         this.repository = repository;
     }
 
+    public Application(){
+        repository = null;
+        System.out.println("Utworzenie application bez repository");
+    }
 
 
+@PostConstruct
     public void start(){
         System.out.println("Start programu");
         repository.read();
+        end();
 
     }
 
@@ -27,5 +36,9 @@ public class Application {
         System.out.println("Koniec programu");
         repository.write();
 
+    }
+//@Autowired
+    public void setRepository(Repository repository) {
+        this.repository = repository;
     }
 }
